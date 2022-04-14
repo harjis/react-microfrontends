497819b9a3df2bd53c02a296aa338d67d39256ec
Joonas Harjukallio <harjukallio@gmail.com>
2022-04-14 08:28:50 +0300

    Doesnt seem to work

--- svelte-content/vite.config.ts
+++ svelte-content/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  server: {
+    port: 3003,
+  },
+  plugins: [
+    svelte(),
+    federation({
+      name: "react_microfrontends_svelte_content",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.svelte",
+      },
+      shared: require("./package.json").dependencies,
+    }),
+  ],
+});
