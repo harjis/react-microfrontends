9e0772f5a23d0fa38cbca4eef688f708617af783
Joonas Harjukallio <harjukallio@gmail.com>
2022-04-14 08:10:18 +0300

    Add svelte-content

---
+++ svelte-content/src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+    background-color: lightgreen;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
+
+<div>
+  <p>I'm Svelte content!</p>
+  <Counter />
+</div>
+
